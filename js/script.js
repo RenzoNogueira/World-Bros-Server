541905cd60fc4696e46ed7fd04b7077cbca5e72b
RenzoNogueira <nogueirarenzoso@gmail.com>
2020-10-21 12:51:51 -0300

    Atualizado painel lateral e corrigido erro em particulas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,129 @@
+
+
+// MEUS TEMPLATES
+
+// Componentes
+Vue.component("menuLateral", {
+    data: function () {
+        return {
+            linksMenuLateral: {
+                home: "index.html",
+                loja: "#",
+                suporte: "suporte.html",
+                galeria: "#",
+                fazerDoacao: "https://server.pro/server/15745071"
+            }
+        }
+    },
+    template: `
+    <div>
+        <center>
+                        <div>
+                            <a class="float-center" href="https://minecraftpocket-servers.com/server/103480/"
+                                target="_blank"><img
+                                    src="https://minecraftpocket-servers.com/server/103480/banners/half-banner-3.png"
+                                    border="0"></a>
+                        </div>
+                        <!-- Discord -->
+                        <div class="mt-3 d-none d-lg-block d-xl-none">
+                            <iframe src="https://discord.com/widget?id=765817125263900673&theme=dark" width="235"
+                                height="200" allowtransparency="true" frameborder="0"
+                                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                        </div>
+                        <div class="mt-3">
+                            <a style="width: 100%;" class="btn btn-primary doacao"
+                                v-bind:href="linksMenuLateral.fazerDoacao" target="_blank" role="button">Fazer
+                                doação <span class="material-icons float-right">
+                                    favorite
+                                </span></a>
+                        </div>
+                    </center>
+
+                    <div class="dropdown mt-3">
+                        <button style="width: 100%;" class="btn btn-secondary dropdown-toggle" type="button"
+                            id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Mais
+                        </button>
+                        <div style="width: 100%;" class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                            <a v-bind:href="linksMenuLateral.home" class="dropdown-item" type="button">Home <span
+                                    class="material-icons float-right">
+                                    home
+                                </span></a>
+                                <div class="dropdown-divider"></div>
+                            <a v-bind:href="linksMenuLateral.loja" class="dropdown-item" type="button">Loja <span
+                                    class="material-icons float-right">
+                                    shopping_cart
+                                </span></a>
+                            <div class="dropdown-divider"></div>
+                            <a v-bind:href="linksMenuLateral.suporte" class="dropdown-item" type="button">Suporte <span
+                                    class="material-icons float-right">
+                                    record_voice_over
+                                </span></a>
+                            <div class="dropdown-divider"></div>
+                            <a v-bind:href="linksMenuLateral.galeria" class="dropdown-item" type="button">Galeria <span
+                                    class="material-icons float-right">
+                                    backup_table
+                                </span></a>
+                        </div>
+                    </div>
+    <div/>
+    `
+})
+
+// INCLUSÃO VUE JS
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        // VARIÁVEIS
+
+        dadosMensagemSuporte: {
+            nick: "",
+            assunto: "",
+            mensagem: "",
+            authentication: false,
+            botaoEnviarSupote: true
+        },
+
+        // DADOS SERVIDOR
+
+        servidor: {
+            enderecoServidor: "worldbros.serv.gs",
+            portaServidor: 40670
+        },
+
+        contatos: {
+            emailServidor: "world.bros@gmail.com",
+            emailDesenvolvedor: "nogueirarenzoso@gmail.com",
+            enderecoDiscord: "discord.gg/kTmJGC"
+        },
+        regrasServidor: [
+            "Não use progamas de terceiros para ter vantagens no jogo, isso não é legal.",
+            "Evite usar palavrões no chat global, é feio."
+        ],
+    },
+
+    methods: {
+        submitToSuporte() {
+            window.open("mailto:" + this.contatos.emailServidor + 
+            "?subject=" + this.dadosMensagemSuporte.assunto + "&&body=My Nick is:" + 
+            this.dadosMensagemSuporte.nick + ". " +
+            this.dadosMensagemSuporte.mensagem, "_blank");
+        }
+    },
+
+    updated: function () {
+        if (this.dadosMensagemSuporte.nick.length > 0 &&
+            this.dadosMensagemSuporte.assunto.length > 0 &&
+            this.dadosMensagemSuporte.mensagem.length > 0 &&
+            this.dadosMensagemSuporte.authentication === true) {
+            this.dadosMensagemSuporte.botaoEnviarSupote = null
+        } else {
+            this.dadosMensagemSuporte.botaoEnviarSupote = true
+        }
+    }
+})
+
 // Particles js
 particlesJS({
     "particles": {
@@ -109,121 +235,3 @@ particlesJS({
     },
     "retina_detect": true
 });
-
-// MEUS TEMPLATES
-
-// Componentes
-Vue.component("menuLateral", {
-    data: function () {
-        return {
-            linksMenuLateral: {
-                loja: "#",
-                suporte: "suporte.html",
-                galeria: "#",
-                fazerDoacao: "https://server.pro/server/15745071"
-            }
-        }
-    },
-    template: `
-    <div>
-        <center>
-                        <div>
-                            <a class="float-center" href="https://minecraftpocket-servers.com/server/103480/"
-                                target="_blank"><img
-                                    src="https://minecraftpocket-servers.com/server/103480/banners/half-banner-3.png"
-                                    border="0"></a>
-                        </div>
-                        <!-- Discord -->
-                        <div class="mt-3 d-none d-lg-block d-xl-none">
-                            <iframe src="https://discord.com/widget?id=765817125263900673&theme=dark" width="235"
-                                height="200" allowtransparency="true" frameborder="0"
-                                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                        </div>
-                        <div class="mt-3">
-                            <a style="width: 100%;" class="btn btn-primary doacao"
-                                v-bind:href="linksMenuLateral.fazerDoacao" target="_blank" role="button">Fazer
-                                doação <span class="material-icons float-right">
-                                    favorite
-                                </span></a>
-                        </div>
-                    </center>
-
-                    <div class="dropdown mt-3">
-                        <button style="width: 100%;" class="btn btn-secondary dropdown-toggle" type="button"
-                            id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Mais
-                        </button>
-                        <div style="width: 100%;" class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                            <a v-bind:href="linksMenuLateral.loja" class="dropdown-item" type="button">Loja <span
-                                    class="material-icons float-right">
-                                    shopping_cart
-                                </span></a>
-                            <div class="dropdown-divider"></div>
-                            <a v-bind:href="linksMenuLateral.suporte" class="dropdown-item" type="button">Suporte <span
-                                    class="material-icons float-right">
-                                    record_voice_over
-                                </span></a>
-                            <div class="dropdown-divider"></div>
-                            <a v-bind:href="linksMenuLateral.galeria" class="dropdown-item" type="button">Galeria <span
-                                    class="material-icons float-right">
-                                    backup_table
-                                </span></a>
-                        </div>
-                    </div>
-    <div/>
-    `
-})
-
-// INCLUSÃO VUE JS
-
-var app = new Vue({
-    el: "#app",
-    data: {
-        // VARIÁVEIS
-
-        dadosMensagemSuporte: {
-            nick: "",
-            assunto: "",
-            mensagem: "",
-            authentication: false,
-            botaoEnviarSupote: true
-        },
-
-        // DADOS SERVIDOR
-
-        servidor: {
-            enderecoServidor: "worldbros.serv.gs",
-            portaServidor: 40670
-        },
-
-        contatos: {
-            emailServidor: "world.bros@gmail.com",
-            emailDesenvolvedor: "nogueirarenzoso@gmail.com",
-            enderecoDiscord: "discord.gg/kTmJGC"
-        },
-        regrasServidor: [
-            "Não use progamas de terceiros para ter vantagens no jogo, isso não é legal.",
-            "Evite usar palavrões no chat global, é feio."
-        ],
-    },
-
-    methods: {
-        submitToSuporte() {
-            window.open("mailto:" + this.contatos.emailServidor + 
-            "?subject=" + this.dadosMensagemSuporte.assunto + "&&body=My Nick is:" + 
-            this.dadosMensagemSuporte.nick + ". " +
-            this.dadosMensagemSuporte.mensagem, "_blank");
-        }
-    },
-
-    updated: function () {
-        if (this.dadosMensagemSuporte.nick.length > 0 &&
-            this.dadosMensagemSuporte.assunto.length > 0 &&
-            this.dadosMensagemSuporte.mensagem.length > 0 &&
-            this.dadosMensagemSuporte.authentication === true) {
-            this.dadosMensagemSuporte.botaoEnviarSupote = null
-        } else {
-            this.dadosMensagemSuporte.botaoEnviarSupote = true
-        }
-    }
-})
